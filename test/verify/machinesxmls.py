1dfa1230017d2c5683a360814a72528ca0d4b2a5
yunmingyang <hjkl0809@163.com>
2020-12-21 15:21:15 +0800

    test: modify for rhel-8.4 ppc64le

--- test/verify/machinesxmls.py
+++ test/verify/machinesxmls.py
@@ -116,12 +116,12 @@ DOMAIN_XML = """
   <name>{name}</name>
   <vcpu>1</vcpu>
   <os>
-    <type arch='x86_64'>hvm</type>
+    <type arch='ppc64le'>hvm</type>
     <boot dev='hd'/>
     <boot dev='network'/>
   </os>
-  <memory unit='MiB'>256</memory>
-  <currentMemory unit='MiB'>256</currentMemory>
+  <memory unit='MiB'>512</memory>
+  <currentMemory unit='MiB'>512</currentMemory>
   <features>
     <acpi/>
   </features>
